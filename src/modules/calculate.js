fcf9c82e6c4f2e892dcfcc5b915885b416e3c5b3
tohvanah <onearth@gmail.com>
2023-07-06 09:47:46 -0500

    Update calculate.js

--- src/modules/calculate.js
+++ src/modules/calculate.js
@@ -28,7 +28,7 @@ const calculate = (ctx, refresh = false, loaded = true) => {
     cols.sort(ctx, children, refresh, loaded);
     return ctx.emit(ctx.constants.EVENT_RECALCULATED);//cols.sort(ctx, children, refresh, loaded);
   }
-
+console.log('MACY CALCULATE : loaded? ' + (loaded? 'true':'false'));
   cols.shuffle(ctx, children, refresh, loaded);
   return ctx.emit(ctx.constants.EVENT_RECALCULATED); //cols.shuffle(ctx, children, refresh, loaded);
 };
